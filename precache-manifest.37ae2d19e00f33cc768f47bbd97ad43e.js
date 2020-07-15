0ea22fefed7d42060a1025e65d6370fd20df3ebc
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-07-15 09:40:09 +0300

    Updates

---
+++ precache-manifest.37ae2d19e00f33cc768f47bbd97ad43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bd9bd4a88e1c084d4af9c312ea4571",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c892538f1eae7f9d2c",
+    "url": "/static/css/main.c407b2ba.chunk.css"
+  },
+  {
+    "revision": "acc0af98bcdac66fe65a",
+    "url": "/static/js/2.e42e8646.chunk.js"
+  },
+  {
+    "revision": "6a37a903df047118eb7c5e2e90a6a2c6",
+    "url": "/static/js/2.e42e8646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c892538f1eae7f9d2c",
+    "url": "/static/js/main.cb689fd5.chunk.js"
+  },
+  {
+    "revision": "786ac4c60d2b47bc6ae0",
+    "url": "/static/js/runtime-main.d8e440b0.js"
+  }
+]);
