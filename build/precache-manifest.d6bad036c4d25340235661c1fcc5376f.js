f20b6319cdcad49900b493d7f255de09702d1053
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-08-09 00:33:27 +0300

    build2

--- build/precache-manifest.d6bad036c4d25340235661c1fcc5376f.js
+++ build/precache-manifest.d6bad036c4d25340235661c1fcc5376f.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "a8da83613586323ee3f267528bb07943",
-    "url": "/react-hexapod-simulator/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "b76ac8fd7abfc4cc8ce2",
-    "url": "/react-hexapod-simulator/static/css/main.c407b2ba.chunk.css"
+    "url": "/static/css/main.c407b2ba.chunk.css"
   },
   {
     "revision": "74b5e95aab4d6f3c66cf",
-    "url": "/react-hexapod-simulator/static/js/2.e29b41a1.chunk.js"
+    "url": "/static/js/2.e29b41a1.chunk.js"
   },
   {
     "revision": "6a37a903df047118eb7c5e2e90a6a2c6",
-    "url": "/react-hexapod-simulator/static/js/2.e29b41a1.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e29b41a1.chunk.js.LICENSE.txt"
   },
   {
     "revision": "b76ac8fd7abfc4cc8ce2",
-    "url": "/react-hexapod-simulator/static/js/main.6e048089.chunk.js"
+    "url": "/static/js/main.6e048089.chunk.js"
   },
   {
     "revision": "44938bd0bd11bc587cc7",
-    "url": "/react-hexapod-simulator/static/js/runtime-main.b871145e.js"
+    "url": "/static/js/runtime-main.b871145e.js"
   }
 ]);
