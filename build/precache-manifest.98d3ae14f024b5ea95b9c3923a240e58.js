453b9e55ba292040bbdbd2a836fe7d3bed3065a5
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-08-12 13:26:48 +0300

    rc build

---
+++ build/precache-manifest.98d3ae14f024b5ea95b9c3923a240e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91117abf9f10bc1c633c3fe121adecdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e34adaef2ac84dc04185",
+    "url": "/static/css/main.73a6c1c3.chunk.css"
+  },
+  {
+    "revision": "fd30859b91f4bc120afd",
+    "url": "/static/js/2.90d1a368.chunk.js"
+  },
+  {
+    "revision": "6a37a903df047118eb7c5e2e90a6a2c6",
+    "url": "/static/js/2.90d1a368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34adaef2ac84dc04185",
+    "url": "/static/js/main.116e494a.chunk.js"
+  },
+  {
+    "revision": "44938bd0bd11bc587cc7",
+    "url": "/static/js/runtime-main.b871145e.js"
+  }
+]);
