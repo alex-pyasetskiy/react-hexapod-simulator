660328178377d328b0165363f2095c007c383859
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-08-12 13:52:54 +0300

    upd

--- server.js
+++ server.js
@@ -30,16 +30,21 @@ SerialPort.list().then(
     err => console.error(err)
   )
 
+function log(msg) {
+    fs.writeFile('socket.log', msg, {'flag':'a'}, function (err) {
+        if (err) return console.error(err);
+    });
+    console.log(msg);
+}
+
 wss.on('connection', function connection(ws, req) {
     // controller.write('#1P1500#2P1500#3P1500#5P1500#6P1500#7P1500#9P1500#10P1500#11P1500#21P1500#22P1500#23P1500#25P1500#26P1500#27P1500#30P1500#31P1500#32P1500T100D500\r\n');
-
+    log(`Connected on port ${wss.port}`)
     ws.on('message', function incoming(message) {
         // console.log('received: %s', message);
         const buf = Buffer.from(`${message}\r\n`)
         controller.write(buf.toString('ascii'));
-        fs.writeFile('socket.log', buf.toString('ascii'), {'flag':'a'}, function (err) {
-            if (err) return console.error(err);
-          });
+        log(`Receive CMD=${buf.toString('ascii')}`);
     });
 
     ws.send('something');
