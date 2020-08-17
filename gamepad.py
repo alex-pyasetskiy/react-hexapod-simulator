38eeeaaa5137b66a0fd7e149e8b347348d4eba0f
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-08-17 23:43:28 +0300

    tmp

---
+++ gamepad.py
@@ -0,0 +1,7 @@
+import inputs
+
+from inputs import get_gamepad
+while 1:
+     events = get_gamepad()
+     for event in events:
+         print(event.ev_type, event.code, event.state)
---
+++ scripts/gamepad.js
@@ -0,0 +1,15 @@
+var Joystick = require("joystick-logitech-f710");
+
+Joystick.create("/dev/input/js0", function (err, joystick) {
+  if (err) {
+    throw err;
+  }
+
+  joystick.on("button:a:press", function () {
+    console.log("jump");
+  });
+
+  joystick.on("button:b:press", function () {
+    console.log("fire");
+  });
+});
