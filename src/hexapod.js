b83696b603ee93d3f43508fd004b3b801e4827a1
Alex Piasetskiy <alex.pyasetskiy@gmail.com>
2020-09-18 06:17:37 +0300

    tm3

--- src/hexapod.js
+++
@@ -1,9 +0,0 @@
-import { solveInverseKinematics, VirtualHexapod, getNewPlotParams } from "./hexapod";
-import * as configs from "./configs"
-
-
-while (true) {
-
-    let res = solveInverseKinematics(configs.DEFAULT_DIMENSIONS, configs.DEFAULT_IK_PARAMS);
-    console.log(res);
-};
